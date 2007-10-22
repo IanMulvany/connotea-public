a204d5e0965d90bb486a0b8efe184a774eb20833
Martin Flack <martin@neoreality.com>
2007-10-23 00:47:18 +0100

    add ability to 304 blog page within 15 mins of update
    darcs-hash:20071022234718-41d90-1e6545063e9da6557020204d5ac9e0334ec29213.gz

--- Bibliotech/Component/Blog.pm
+++ Bibliotech/Component/Blog.pm
@@ -11,11 +11,21 @@ package Bibliotech::Component::Blog;
 use strict;
 use base 'Bibliotech::Component';
 use XML::Feed;
+use Bibliotech::Util;
 
 our $FEED_URL = URI->new(__PACKAGE__->cfg('FEED_URL') || 'file:///tmp/blog.xml');
 
-sub last_updated_basis {
-  'NOW';
+sub last_updated {
+  # since this comes from the network we cannot supply the
+  # last_updated timestamp without fetching it so just for the sake of
+  # 304'ing we are just going to say it was updated within the last 15
+  # minutes if we fetched the blog within the last 15 minutes
+  my $self   = shift;
+  my $key    = Bibliotech::Cache::Key->new(class => __PACKAGE__, method => 'html_content');
+  my $cached = $self->bibliotech->memcache->get($key);
+  my $time   = defined $cached ? $cached->last_updated : undef;
+  return $time if defined $time and $time > Bibliotech::Util::time() - 900;
+  return 'NOW';
 }
 
 sub lazy_update {
@@ -30,8 +40,7 @@ sub get_feed {
 
 sub html_content {
   my ($self, $class, $verbose, $main) = @_;
-  my $cached = $self->memcache_check(class  => __PACKAGE__,
-				     method => 'html_content');
+  my $cached = $self->memcache_check(class  => __PACKAGE__, method => 'html_content');
   if (defined $cached) {
     $self->discover_main_title($cached->content);
     return $cached;
