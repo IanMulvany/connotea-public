fbdc5658a8e45c8b930d954818f4afb53bbc0ab9
Martin Flack <martin@neoreality.com>
2007-11-08 05:24:24 +0000

    add delorphans.pl for bookmarks
    darcs-hash:20071108052424-41d90-546472867b5856fb485f8a530dab3029c8b0508d.gz

---
+++ sql/delorphans.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use strict;
+use Bibliotech::DBI;
+
+# remove bookmarks that have no postings (and their linked citations)
+
+my $dbh = Bibliotech::DBI->db_Main();
+my $sth = $dbh->prepare('select bookmark_id from bookmark b left join user_bookmark ub on (b.bookmark_id=ub.bookmark) where ub.user_bookmark_id is null');
+$sth->execute;
+while (my ($bookmark_id) = $sth->fetchrow_array) {
+  print "del $bookmark_id\n";
+  Bibliotech::Bookmark->retrieve($bookmark_id)->delete;
+}
