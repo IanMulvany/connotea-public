3ce910685def080edaf9cd7b30e34bda491dae07
Martin Flack <martin@neoreality.com>
2007-11-08 22:36:34 +0000

    add row count to delorphans.pl
    darcs-hash:20071108223634-41d90-c9c3c93ea54ad8d3e28982bb8fcc19f7b3b5fc6a.gz

--- sql/delorphans.pl
+++ sql/delorphans.pl
@@ -7,6 +7,7 @@ use Bibliotech::DBI;
 my $dbh = Bibliotech::DBI->db_Main();
 my $sth = $dbh->prepare('select bookmark_id from bookmark b left join user_bookmark ub on (b.bookmark_id=ub.bookmark) where ub.user_bookmark_id is null');
 $sth->execute;
+print $sth->rows." bookmarks\n";
 while (my ($bookmark_id) = $sth->fetchrow_array) {
   print "del $bookmark_id\n";
   my $bookmark = Bibliotech::Bookmark->retrieve($bookmark_id);
