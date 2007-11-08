d1b1d4fc15bddb1ce202e2c59b17dbee1b651668
Martin Flack <martin@neoreality.com>
2007-11-08 22:59:54 +0000

    print url and catch sigint in delorphans.pl
    darcs-hash:20071108225954-41d90-f2fe441b26dff206d3d4ce39062646eacf7e1737.gz

--- sql/delorphans.pl
+++ sql/delorphans.pl
@@ -4,14 +4,19 @@ use Bibliotech::DBI;
 
 # remove bookmarks that have no postings (and their linked citations)
 
+my $INTFLAG = 0;
+$SIG{INT} = sub { warn "SIGINT detected\n"; $INTFLAG++; };
+
 my $dbh = Bibliotech::DBI->db_Main();
 my $sth = $dbh->prepare('select bookmark_id from bookmark b left join user_bookmark ub on (b.bookmark_id=ub.bookmark) where ub.user_bookmark_id is null');
 $sth->execute;
 print $sth->rows." bookmarks\n";
 while (my ($bookmark_id) = $sth->fetchrow_array) {
+  exit 1 if $INTFLAG;
   print "del $bookmark_id ";
   eval { my $bookmark = Bibliotech::Bookmark->retrieve($bookmark_id);
 	 print $bookmark->url."\n";
-	 $bookmark->delete; };
+	 #$bookmark->delete;
+  };
   warn $@ if $@;
 }
