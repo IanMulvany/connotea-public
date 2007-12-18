d7ec6b958885914beae4c828967264b4ddc2abba
Martin Flack <martin@neoreality.com>
2007-12-18 05:42:34 +0000

    add is_dup_md5 for captcha
    darcs-hash:20071218054234-41d90-7faa264e3d57855dbf9a2fa1c28e10766000eb07.gz

--- Bibliotech/Captcha.pm
+++ Bibliotech/Captcha.pm
@@ -10,6 +10,9 @@
 package Bibliotech::Captcha;
 use strict;
 use base 'Authen::Captcha';
+use Digest::MD5 qw(md5_hex);
+use Carp;
+use File::Spec;
 use Bibliotech::Config;
 
 our $DATA_FOLDER     = Bibliotech::Config->get('CAPTCHA', 'DATA_FOLDER')     || '/tmp/captcha/data';
@@ -26,4 +29,37 @@ sub new {
 sub img_href {
   my $md5sum = pop;
   return $OUTPUT_LOCATION.$md5sum.'.png';
+}
+
+sub is_dup_md5 {
+  ref(my $self = shift) or croak "instance variable needed";
+  my $md5 = shift;
+  my $match_md5 = qr|^\d+::$md5$|;
+  my $database_file = File::Spec->catfile($self->data_folder(),'codes.txt');
+  open (DATA, "<$database_file")  or die "Can't open File: $database_file\n";
+  flock DATA, 1;  # read lock
+  my $conflict = eval {
+    while (<DATA>) {
+      return 1 if /$match_md5/;
+    }
+    return 0;
+  };
+  die $@ if $@;
+  flock DATA, 8;
+  close(DATA);
+  return $conflict;
+}
+
+# wrap the official version with a loop to skip dup's
+# rather than treat them as the official version does
+sub generate_random_string {
+  ref(my $self = shift) or croak "instance variable needed";
+  my $length = shift;
+  my ($code, $conflict);
+  my $counter = 0;
+  do {
+    $code = $self->SUPER::generate_random_string($length);
+    $conflict = $self->is_dup_md5(md5_hex($code));
+  } while ($conflict and ++$counter < 100);
+  return $code;
 }
