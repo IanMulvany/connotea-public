a05ad71ea47f90c73b598596145f916c9146e378
Martin Flack <martin@neoreality.com>
2008-03-12 01:14:22 +0000

    add /etc/hosts lookup and white/black list config to UserAgent.pm
    darcs-hash:20080312011422-41d90-4b1411f007781f1b2ce211564b3df6eb235d4414.gz

--- Bibliotech/UserAgent.pm
+++ Bibliotech/UserAgent.pm
@@ -10,13 +10,22 @@ package Bibliotech::UserAgent;
 use strict;
 use base 'LWPx::ParanoidAgent';
 use Bibliotech::Util;
+use Bibliotech::Config;
 
-our $SITE_NAME;
-sub config_site_name {
-  return $SITE_NAME if defined $SITE_NAME;
-  eval "use Bibliotech::Config";
-  die $@ if $@;
-  return $SITE_NAME = Bibliotech::Config->get('SITE_NAME');
+our $SITE_NAME 	= Bibliotech::Config->get('SITE_NAME');
+our $STRING    	= Bibliotech::Config->get('AGENT', 'STRING');
+our $TIMEOUT   	= Bibliotech::Config->get('AGENT', 'TIMEOUT') || 180;
+our $WHITE_LIST = Bibliotech::Config->get('AGENT', 'WHITE_LIST') || [];
+our $BLACK_LIST = Bibliotech::Config->get('AGENT', 'BLACK_LIST') || [];
+
+sub _make_qr {
+  local $_ = shift;
+  /^((\d+\.){1,3})$/ and return qr/^$1/;
+  return $_;
+}
+
+sub _make_qrs {
+  map { ref($_) ? $_ : _make_qr($_) } @_;
 }
 
 # create normal object but override sitename
@@ -25,9 +34,11 @@ sub new {
   my $bibliotech = $options{bibliotech};
   delete $options{bibliotech};
   my $self = $class->SUPER::new(%options);
-  my $sitename = defined $bibliotech ? $bibliotech->sitename : config_site_name();
-  $self->timeout(180);
-  $self->agent($sitename.' ');  # trailing space ensures LWP will add version info
+  my $sitename = defined $bibliotech ? $bibliotech->sitename : $SITE_NAME;
+  $self->timeout($TIMEOUT);
+  $self->agent($STRING || $sitename.' ');  # trailing space ensures LWP will add version info
+  $self->blocked_hosts(_make_qrs(@{$BLACK_LIST}))     if $BLACK_LIST and @{$BLACK_LIST};
+  $self->whitelisted_hosts(_make_qrs(@{$WHITE_LIST})) if $WHITE_LIST and @{$WHITE_LIST};
   return $self;
 }
 
@@ -48,6 +59,29 @@ sub request {
     $response->content($decoded);
   }
   return $response;
+}
+
+sub system_hosts_file {
+  open HOSTS, '</etc/hosts' or die 'unable to open /etc/hosts: '.$!;
+  my @hosts = <HOSTS>;
+  close HOSTS;
+  return @hosts;
+}
+
+# look in /etc/hosts
+sub _resolve {
+  my ($self, $host, $request, $timeout, $depth) = @_;
+  unless ($host =~ /^\d+\.\d+\.\d+\.\d+$/) {
+    foreach (system_hosts_file()) {
+      if (/^(\d+\.\d+\.\d+\.\d+)(\s+[\w\.]+)+$/) {
+	my ($ip, @names) = ($1, grep { $_ } split(/\s+/, $2));
+	if (grep { $host eq $_ } @names) {
+	  return $self->SUPER::_resolve($ip, $request, $timeout, $depth);
+	}
+      }
+    }
+  }
+  return $self->SUPER::_resolve($host, $request, $timeout, $depth);
 }
 
 # override the user agent used for HTTP::OAI as well
