5a8e9c30d91694dba119bdb7e24a1dbfea3d5cf0
Martin Flack <martin@neoreality.com>
2008-03-19 04:31:45 +0000

    fix strange dates in Atypon RIS files and add support for banner-less pages
    darcs-hash:20080319043145-41d90-63531bcf64dbf5774f913659ab389ee64a64ca81.gz

--- Bibliotech/CitationSource/Atypon.pm
+++ Bibliotech/CitationSource/Atypon.pm
@@ -50,6 +50,7 @@ package Bibliotech::CitationSource::Atypon;
 use base 'Bibliotech::CitationSource';
 use URI;
 use URI::QueryParam;
+use DateTime;
 
 sub api_version {
   1;
@@ -64,7 +65,8 @@ sub understands {
   return 0 unless $uri->scheme eq 'http';
   return 0 unless _doi_from_uri($uri);
   local $_ = $self->content_or_set_warnstr($content_sub, ['text/html', 'application/xhtml+xml']) or return 0;
-  m|<a href="http://www.atypon.com| or return 0;  # match "Technology Partner" banner
+  return 0 unless m|<a href="http://www.atypon.com| or       # match "Technology Partner" banner
+                  m|<a href="/action/showCitFormats\?doi=|;  # match "Download to Citation Mgr" link
   return 3;
 }
 
@@ -85,6 +87,20 @@ sub _query_uri {
   return $q;
 }
 
+sub _fix_date {
+  local $_ = shift;
+  my ($year, $month, $day) = m|^(\d+)/(\d+)/(\d+)$|;
+  return $_ if $day <= 31;
+  # fix bug where day is day-of-year (1-365)
+  return DateTime->from_day_of_year(year => $year, day_of_year => $day)->ymd;
+}
+
+sub _fix_ris_date {
+  local $_ = shift;
+  s|^(Y1  - )(\d+/\d+/\d+)|$1._fix_date($2)|em;
+  return $_;
+}
+
 sub citations {
   my ($self, $uri, $content_sub) = @_;
   my $ris = eval {
@@ -92,7 +108,7 @@ sub citations {
     my $doi = _doi_from_uri($uri) or die "no DOI\n";
     my ($res, $ris_raw) = $self->get(_query_uri($uri->host, $doi));
     $res->is_success or 'trying to get RIS: '.$res->status_line."\n";
-    my $ris = Bibliotech::CitationSource::NPG::RIS->new($ris_raw) or die "no RIS object\n";
+    my $ris = Bibliotech::CitationSource::NPG::RIS->new(_fix_ris_date($ris_raw)) or die "no RIS object\n";
     $ris->has_data or die "RIS file contained no data\n";
     return $ris;
   };
