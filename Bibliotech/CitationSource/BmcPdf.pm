144637b37c35e8d05d5245c19291a61419fdf313
Martin Flack <martin@neoreality.com>
2008-07-18 23:28:04 +0100

    refactor code formatting in BmcPdf citation source module
    darcs-hash:20080718222804-41d90-107328a61cc595dffcbff2bac3fd50496577112e.gz

--- Bibliotech/CitationSource/BmcPdf.pm
+++ Bibliotech/CitationSource/BmcPdf.pm
@@ -14,22 +14,18 @@ use warnings;
 
 use Bibliotech::CitationSource;
 use base 'Bibliotech::CitationSource';
-
 use Bibliotech::CitationSource::Simple;
 use Bibliotech::CitationSource::autodiscovery;
 
-sub api_version
-{
+sub api_version {
   1;
 }
 
-sub name
-{
+sub name {
   'BmcPdf';
 }
 
-sub version
-{
+sub version {
   '1.1.2.1';
 }
 
@@ -37,46 +33,38 @@ sub potential_understands {
   2;
 }
 
-sub understands
-{
-    my ($self, $uri) = @_;
-
-	return 0 unless ($uri->scheme =~ /^http$/i);
-
-	# check it looks like a BMC PDF
-        return 0 unless ($uri->path =~ m/^\/content\/pdf\/.*\.pdf$/);
-	
-	# might be able to do something, so return 2
-	return 2;	     
+sub understands {
+  my ($self, $uri) = @_;
+  return 0 unless $uri->scheme =~ /^http$/i;
+  return 0 unless $uri->path =~ m/^\/content\/pdf\/.*\.pdf$/;  # check it looks like a BMC PDF
+  return 2;  # might be able to do something, so return 2
 }
 
-sub citations
-{
-	my ($self, $uri) = @_;
+sub citations {
+  my ($self, $uri) = @_;
 
-	return undef unless $self->understands($uri);
-	
-	#translate to abstract URL
-	if ($uri->path =~ m/^\/content\/pdf\/(.+?-.+?)-(.+?)-(.+?)\.pdf$/) {
-	    my $abs_html_url = 'http://' . $uri->host . '/';
-	    #differnt rules for non-BMC.com hosts
-	    my ($one, $two, $three) = ($1, $2, $3);
-	    if ($uri->host =~ m/^(www\.)?biomedcentral\.com$/) {
-		$abs_html_url .= $one . '/' . $two . '/' . $three . '/abstract';
-	    }
-	    else {
-		$abs_html_url .= 'content/' . $two . '/1/' . $three . '/abstract';
-	    }
+  return undef unless $self->understands($uri);
 
-	    my $ad = Bibliotech::CitationSource::autodiscovery->new($self->bibliotech);
-	    return $ad->citations(URI->new($abs_html_url));
-	}
-	else {
-	    $self->errstr("Couldn't convert PDF URL to abstract HTML URL");
-	    return undef
-	}
-	
+  # translate to abstract URL
+  if ($uri->path =~ m/^\/content\/pdf\/(.+?-.+?)-(.+?)-(.+?)\.pdf$/) {
+    my $abs_html_url = 'http://' . $uri->host . '/';
+    #differnt rules for non-BMC.com hosts
+    my ($one, $two, $three) = ($1, $2, $3);
+    if ($uri->host =~ m/^(www\.)?biomedcentral\.com$/) {
+      $abs_html_url .= $one . '/' . $two . '/' . $three . '/abstract';
+    }
+    else {
+      $abs_html_url .= 'content/' . $two . '/1/' . $three . '/abstract';
+    }
+
+    my $ad = Bibliotech::CitationSource::autodiscovery->new($self->bibliotech);
+    return $ad->citations(URI->new($abs_html_url));
+  }
+  else {
+    $self->errstr("Couldn't convert PDF URL to abstract HTML URL");
+    return undef;
+  }
 }
 
-#true!
 1;
+__END__
