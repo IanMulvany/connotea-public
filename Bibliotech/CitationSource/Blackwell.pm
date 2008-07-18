5d77ec8833ee94fb2b3fda325836fb98ef28b770
Martin Flack <martin@neoreality.com>
2008-07-19 00:32:13 +0100

    refactor code formatting in Blackwell citation source module
    darcs-hash:20080718233213-41d90-c26e49ab7905fcc902f1911f74045e93892b74b7.gz

--- Bibliotech/CitationSource/Blackwell.pm
+++ Bibliotech/CitationSource/Blackwell.pm
@@ -7,11 +7,6 @@ package Bibliotech::CitationSource::Blackwell;
 use base 'Bibliotech::CitationSource';
 use URI;
 use URI::QueryParam;
-use Data::Dumper;
-
-#
-# Site depends on session cookie
-#
 use LWP;
 use HTTP::Request::Common;
 use HTTP::Cookies;
@@ -24,21 +19,17 @@ sub name {
   'Blackwell';
 }
 
-
 sub understands {
   my ($self, $uri) = @_;
+  return 0 unless $uri->scheme =~ /^http$/i;
+  return 0 unless $uri->host =~ m/^(www\.)?blackwell-synergy\.com$/;
 
-  #check the host
-  return 0 unless ($uri->scheme =~ /^http$/i);
-  return 0 unless ($uri->host =~ m/^(www\.)?blackwell-synergy\.com$/);
-
-  #	ex: doi/abs/10.1111/j.1600-0501.2005.01111.x
+  # ex: doi/abs/10.1111/j.1600-0501.2005.01111.x
   # ex: doi/10.1111/j.1523-1755.2005.00267.x/full
-  return 1 if ($uri->path =~ m!/doi/.+!i);
+  return 1 if $uri->path =~ m!/doi/.+!i;
 
-  # ex.
-  # http://www.blackwell-synergy.com/servlet/useragent?func=synergy&synergyAction=showAbstract&doi=10.1046/j.1365-2486.2002.00492.x
-  return 1 if ($uri->query_param('doi'));
+  # ex. http://www.blackwell-synergy.com/servlet/useragent?func=synergy&synergyAction=showAbstract&doi=10.1046/j.1365-2486.2002.00492.x
+  return 1 if $uri->query_param('doi');
 
   return 0;
 }
@@ -54,48 +45,45 @@ sub citations {
 
     die "no file name seen in URI\n" unless $file;
 
-	#print "FILE: $file\n";
+    my ($id);
+    if ($article_uri->query && $article_uri->query_param('doi')) {
+      $id = $article_uri->query_param('doi');
+    } else {
+      # ex: 10.1111/j.1523-1755.2005.00267.x
+      # ex: 10.1111/j.1523-1755.2005.00267.x/full
+      # ignore anything after the second slash if it exists (doi has on slash in it)
+      ($id) = $file =~ /(\d.*?\/.*?)(\/.*?)?$/;
+    }
 
-	my($id);
-	if($article_uri->query && $article_uri->query_param('doi')) {
-	    $id = $article_uri->query_param('doi');
-	} else {
-  		# ex: 10.1111/j.1523-1755.2005.00267.x
-  		# ex: 10.1111/j.1523-1755.2005.00267.x/full
-		# ignore anything after the second slash if it exists (doi has on slash in it)
-		($id) = $file =~ /(\d.*?\/.*?)(\/.*?)?$/;
-	}
-	#print "ID: $id\n";
+    die "no doi\n" unless $id;	# will set errstr, die
 
-	die "no doi\n" unless $id;	# will set errstr, die
+    my $cookieJar = new HTTP::Cookies();
+    my $ua = $self->ua;
+    $ua->cookie_jar($cookieJar);
 
-	my $cookieJar = new HTTP::Cookies();
-	my $ua = $self->ua;
-	$ua->cookie_jar($cookieJar);
+    #
+    # set/get session cookie (among others that may be automatically set)
+    #
+    my $res = $ua->request(GET 'http://'.$article_uri->host);
 
-	#
-	# set/get session cookie (among others that may be automatically set)
-	#
-	my $res = $ua->request(GET "http://" . $article_uri->host);
+    #
+    # check for problem with request
+    #
+    if ($res->is_success) {
+      $res = $ua->request(POST 'http://'.$article_uri->host.'/action/downloadCitation',
+			  ['doi'     => $id,
+			   'include' => 'cit',
+			   'format'  => 'refman',
+			   'direct'  => 'checked',
+			   'submit'  => 'Download references']);
 
-	#
-	# check for problem with request
-	#
-	if($res->is_success) {
-		$res = $ua->request(POST "http://" . $article_uri->host . "/action/downloadCitation", [
-				 'doi' => $id,
-				 'include' => 'cit',
-				 'format' => 'refman',
-				 'direct' => 'checked',
-				 'submit' => 'Download references']);
-
-		$ris = new Bibliotech::CitationSource::NPG::RIS ($res->content);
-		die "RIS obj false\n" unless $ris;
-		die "RIS file contained no data\n" unless $ris->has_data;
-		$ris->{M3} = $id;
-	} else {
-	  die $res->status_line;
-    	}
+      $ris = Bibliotech::CitationSource::NPG::RIS->new($res->content);
+      die "RIS obj false\n" unless $ris;
+      die "RIS file contained no data\n" unless $ris->has_data;
+      $ris->{M3} = $id;
+    } else {
+      die $res->status_line;
+    }
   };    
   die $@ if $@ =~ /at .* line \d+/;
   $self->errstr($@), return undef if $@;
@@ -182,7 +170,6 @@ sub new {
   $self->firstname($firstname);
   $self->lastname($lastname);
   $self->initials($initials);
-  
   return $self;
 }
 
