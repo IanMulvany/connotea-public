7b14c83de4ecf392b3451e0670fc7701d58021fd
Martin Flack <martin@neoreality.com>
2008-09-12 20:26:27 +0100

    move resignation form to compresign.tt
    darcs-hash:20080912192627-41d90-0de510efce81251fe448a6dca2754b5cc92fc009.gz

--- Bibliotech/Component/ResignForm.pm
+++ Bibliotech/Component/ResignForm.pm
@@ -22,56 +22,36 @@ sub html_content {
   my $user       = $self->getlogin or return $self->saylogin('to resign');
   my $bibliotech = $self->bibliotech;
   my $cgi        = $bibliotech->cgi;
-  my $validationmsg;
+  my $validation;
 
   if ($cgi->request_method eq 'POST') {
-    my $button = lc($cgi->param('button'));
-    if ($button eq 'cancel') {
-      die "Location: ".$bibliotech->location."library\n";
-    }
-    elsif ($button eq 'resign') {
-      eval {
-	die "You must confirm your choice by selecting Yes.\n" if $cgi->param('confirm') ne 'yes';
-	if (my $comment = $self->cleanparam($cgi->param('comment'))) {
-	  $bibliotech->notify_admin(body => 'User '.$user->username." leaves this comment upon resignation:\n\n$comment\n");
-	}
-	$bibliotech->user->deactivate($bibliotech, 'resignation');
-      };
-      if ($@) {
-	$validationmsg = $@;
+    my $button = $cgi->param('button');
+    if ($button) {
+      my $confirm = $cgi->param('confirm');
+      if (lc($button) eq 'cancel') {
+	die "Location: ".$bibliotech->location."library\n";
       }
-      else {
-	die 'Location: '.Bibliotech::Component::LogoutForm->do_logout_and_return_location($bibliotech)."\n";
+      elsif (lc($button) eq 'resign') {
+	eval {
+	  die $self->validation_exception(confirm => "Resignation will not be accepted unless you confirm with Yes.\n")
+	      unless $confirm and lc($confirm) eq 'yes';
+	  if (my $comment = $self->cleanparam($cgi->param('comment'))) {
+	    $bibliotech->notify_admin(body => 'User '.$user->username." leaves this comment upon resignation:\n\n$comment\n");
+	  }
+	  $user->deactivate($bibliotech, 'resignation');
+	};
+	if ($@) {
+	  die $@ if $@ =~ /at .* line \d+/;
+	  $validation = $self->validation_exception(undef => $@);
+	}
+	else {
+	  die 'Location: '.Bibliotech::Component::LogoutForm->do_logout_and_return_location($bibliotech)."\n";
+	}
       }
     }
   }
 
-  my $o = '';
-
-  $o .= $cgi->div({class => 'errormsg'}, $validationmsg) if $validationmsg;
-
-  $o .= $cgi->div($cgi->h1('Resign Account'),
-		  $cgi->start_form(-method => 'POST', -action => $bibliotech->location.'resign'),
-		  $cgi->p('This action will resign the '.$bibliotech->sitename.' user '.$cgi->b($user->username).'.'),
-		  $cgi->p('Are you sure that you want to delete your account?'),
-		  $cgi->popup_menu(-name    => 'confirm',
-				   -id      => 'confirmselect',
-				   -values  => [qw/no yes/],
-				   -labels  => {'no'  => 'No',
-						'yes' => 'Yes',
-				               },
-				   -default => 'no'),
-		  $cgi->p('If you would like to leave a comment for our team as you resign, you may optionally do so here:'),
-		  $cgi->textarea(-id => 'commentbox', -class => 'searchtextctl', -name => 'comment', -cols => 60, -rows => 5),
-		  $cgi->br,
-		  $cgi->submit(-id => 'submitbutton', -class => 'buttonctl', -name => 'button', -value => 'Resign'),
-		  $cgi->submit(-id => 'cancelbutton', -class => 'buttonctl', -name => 'button', -value => 'Cancel'),
-		  $cgi->end_form,
-		  );
-
-  $self->discover_main_title($o);
-
-  return Bibliotech::Page::HTML_Content->simple($o);
+  return Bibliotech::Page::HTML_Content->simple($self->tt('compresign', undef, $validation));
 }
 
 1;
